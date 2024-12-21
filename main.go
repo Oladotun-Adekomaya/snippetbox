cbe012e740aa11de9186515d1f14df55cd3860bc
oladotun <oladotunadekomaya@gmail.com>
2024-12-21 14:20:11 +0100

    commit

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		w.WriteHeader(405)
-		fmt.Fprint(w, "Method Not Allowed")
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
