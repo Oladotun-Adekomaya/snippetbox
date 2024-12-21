f39ba533c7246cb46e7245fb6a9df6fddff111c2
oladotun <oladotunadekomaya@gmail.com>
2024-12-21 13:36:35 +0100

    commit

--- main.go
+++ main.go
@@ -14,11 +14,15 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Display a specific snippet...")
 }
 
-func snippetCreate()
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Create a new snippet...")
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
