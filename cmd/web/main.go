c0c1180fd811e3d497f3d18f3e8bf5283e3338b5
oladotun <oladotunadekomaya@gmail.com>
2024-12-24 04:48:12 +0100

    commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,12 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	// To serve files a directory
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Handle requests to file path
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Println(err)
